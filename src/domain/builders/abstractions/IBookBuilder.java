38fc9cb38a2d800d24dd97b4e82fed3afb632d67
popescupaula <70951990+popescupaula@users.noreply.github.com>
2021-12-16 22:48:13 +0200

    Delete IBookBuilder.java

--- src/domain/builders/abstractions/IBookBuilder.java
+++
@@ -1,9 +0,0 @@
-package domain.builders.abstractions;
-
-public interface IBookBuilder {
-    void create();
-    void setName(String name);
-    void setAuthor(String author);
-    void setPage(int page);
-    void setPrice(double price);
-}
