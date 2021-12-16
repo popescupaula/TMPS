ac636877531bc3f2614a23e65aee5a1e2bf4f101
popescupaula <70951990+popescupaula@users.noreply.github.com>
2021-12-16 22:50:13 +0200

    Delete src/domain/factories directory

--- src/domain/factories/abstractions/IAbstractFactory.java
+++
@@ -1,7 +0,0 @@
-package domain.factories.abstractions;
-
-import models.books.abstractions.IBook;
-
-public interface IAbstractFactory {
-    IBook sellBook(String name, boolean needsCustomPackage);
-}
